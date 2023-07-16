63f0017ccc8d3133ceec7588075895ea48db0e38
Evren Kayali <EvrenKayali@hotmail.com>
2023-07-16 15:04:52 +1000

    feat: store hashed passwords in db

--- lib/userManager.ts
+++ lib/userManager.ts
@@ -1,9 +1,17 @@
 import { NewUser, db } from "@/data/drizzle";
 import { users } from "@/data/entities";
 import { and, eq } from "drizzle-orm";
+import * as bcrypt from "bcrypt";
 
 export const register = async (user: NewUser) => {
-  const result = await db.insert(users).values(user).returning();
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const passwordHash = await bcrypt.hash(user.password!, salt);
+
+  const result = await db
+    .insert(users)
+    .values({ ...user, password: passwordHash })
+    .returning();
   return result[0];
 };
 
@@ -11,7 +19,15 @@ export const findByUserNameAndPassword = async (
   email: string,
   password: string
 ) => {
-  return await db.query.users.findFirst({
-    where: and(eq(users.email, email), eq(users.password, password)),
+  const user = await db.query.users.findFirst({
+    where: eq(users.email, email),
   });
+
+  if (!user) {
+    return undefined;
+  }
+
+  if (await bcrypt.compare(password, user.password)) {
+    return user;
+  }
 };
