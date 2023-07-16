1c7ab58c79b46b7398a490b1cbc4911e05b48bb0
Evren Kayali <EvrenKayali@hotmail.com>
2023-07-16 23:10:24 +1000

    feat: add skeleton

--- app/(auth)/me/loading.tsx
+++ app/(auth)/me/loading.tsx
@@ -1,7 +1,12 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
   return (
     <main className="container mt-4">
-      <div className="bg-secondary shadow-xl p-4">Loading....</div>
+      <div className="bg-secondary shadow-xl p-5">
+        <Skeleton className="h-4 w-[250px] bg-gray-300 mb-2" />
+        <Skeleton className="h-4 w-[200px] bg-gray-300" />
+      </div>
     </main>
   );
 }
